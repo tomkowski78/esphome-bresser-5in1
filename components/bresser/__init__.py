1344ac2d6566b19cb5920000405318e2c01dca8e
Aleksander Data <54954537+olekdata@users.noreply.github.com>
2025-03-11 20:08:35 +0100

    init

--- components/bresser/__init__.py
+++ components/bresser/__init__.py
@@ -1,22 +1,68 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_CELSIUS, DEVICE_CLASS_TEMPERATURE, STATE_CLASS_MEASUREMENT
+from esphome import pins
+from esphome.const import (
+    CONF_HUMIDITY,
+    CONF_WIND_SPEED,
+    CONF_WIND_DIRECTION_DEGREES,
+    CONF_ID,
+    CONF_TEMPERATURE,
+    CONF_PIN,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_WIND_SPEED,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_PERCENT,
+    UNIT_DEGREES,
+    UNIT_MILLIMETER,
+)
 
-DEPENDENCIES = []
+CONF_PIN_CS      = "PIN_CS"
+CONF_PIN_GDO0    = "PIN_GDO0"
+CONF_PIN_GDO2    = "PIN_GDO2"
 
-my_sensor_ns = cg.esphome_ns.namespace('my_sensor')
-MySensor = my_sensor_ns.class_('MySensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    MySensor,
-    unit_of_measurement=UNIT_CELSIUS,
-    device_class=DEVICE_CLASS_TEMPERATURE,
-    state_class=STATE_CLASS_MEASUREMENT
-).extend({})
+PIN_CS   = 5
+PIN_GDO0 = 27
+PIN_GDO2 =  4
+
+
+bresser_ns = cg.esphome_ns.namespace("bresser")
+Bresser = bresser_ns.class_(
+    "Bresser", cg.PollingComponent 
+)
+
+
+
+CONFIG_SCHEMA = cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Bresser),
+            
+            cv.Required(CONF_PIN_CS): cv.All(pins.internal_gpio_input_pin_schema),
+            cv.Required(CONF_PIN_GDO0): cv.All(pins.internal_gpio_input_pin_schema),
+            cv.Required(CONF_PIN_GDO2): cv.All(pins.internal_gpio_input_pin_schema),
+        }
+    ).extend(cv.polling_component_schema("60s"))
+
 
 async def to_code(config):
-    var = cg.new_Pvariable(config)
+    var  = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
-    cg.add(var.set_update_interval(1000))
+
+    #await sensor.register_sensor(var, config) 
+    #await sensor.my_to_code_sensor(var, config)
+   
+    pin = await cg.gpio_pin_expression(config[CONF_PIN_CS])
+    cg.add(var.set_pin_cs(pin))
+    
+    pin = await cg.gpio_pin_expression(config[CONF_PIN_GDO0])
+    cg.add(var.set_pin_gdo0(pin))
+    
+    pin = await cg.gpio_pin_expression(config[CONF_PIN_GDO2])
+    cg.add(var.set_pin_gdo2(pin))
+    
+#    var_adv = cg.new_Pvariable(config[CONF_INFO_COMP_ID])
+#    await cg.register_component(var_adv, {})
+
--- components/bresser/sensor.py
+++ components/bresser/sensor.py
@@ -1,21 +1,92 @@
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, UNIT_CELSIUS, DEVICE_CLASS_TEMPERATURE, STATE_CLASS_MEASUREMENT
+from esphome import pins
+from esphome.const import (
+    CONF_HUMIDITY,
+    CONF_WIND_SPEED,
+    CONF_WIND_DIRECTION_DEGREES,
+    CONF_ID,
+    CONF_TEMPERATURE,
+    CONF_PIN,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_WIND_SPEED,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_PERCENT,
+    UNIT_DEGREES,
+    UNIT_MILLIMETER,
+)
 
-my_sensor_ns = cg.esphome_ns.namespace('my_sensor')
-MySensor = my_sensor_ns.class_('MySensor', sensor.Sensor, cg.PollingComponent)
+bresser_ns = cg.esphome_ns.namespace("bresser")
+Bresser = bresser_ns.class_(
+    "Bresser", cg.PollingComponent 
+)
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    MySensor,
-    unit_of_measurement=UNIT_CELSIUS,
-    device_class=DEVICE_CLASS_TEMPERATURE,
-    state_class=STATE_CLASS_MEASUREMENT
-).extend({
-    cv.GenerateID(): cv.declare_id(MySensor),
-})
+CONFIG_SCHEMA = cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(Bresser),
+            
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_HUMIDITY,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_WIND_SPEED): sensor.sensor_schema(
+                unit_of_measurement="m/s",
+                accuracy_decimals=1,
+                #device_class=DEVICE_CLASS_WIND_SPEED,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_WIND_DIRECTION_DEGREES): sensor.sensor_schema(
+                unit_of_measurement=UNIT_DEGREES,
+                accuracy_decimals=0,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional('rain'): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MILLIMETER,
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional('rain'): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MILLIMETER,
+                accuracy_decimals=1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+
+        }
+    ).extend(cv.polling_component_schema("60s"))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
+
+    if temperature_config := config.get(CONF_TEMPERATURE):
+        sens = await sensor.new_sensor(temperature_config)
+        cg.add(var.set_temperature_sensor(sens))
+        
+    if humidity_config := config.get(CONF_HUMIDITY):
+        sens = await sensor.new_sensor(humidity_config)
+        cg.add(var.set_humidity_sensor(sens))
+        
+    if wind_speed_config := config.get(CONF_WIND_SPEED):
+        sens = await sensor.new_sensor(wind_speed_config)
+        cg.add(var.set_wind_speed_sensor(sens))        
+        
+    if wind_direction_degrees_config := config.get(CONF_WIND_DIRECTION_DEGREES):
+        sens = await sensor.new_sensor(wind_direction_degrees_config)
+        cg.add(var.set_wind_direction_sensor(sens))                
+        
+    if rain_config := config.get('rain'):
+        sens = await sensor.new_sensor(rain_config)
+        cg.add(var.set_rain_sensor(sens))                        
+        
